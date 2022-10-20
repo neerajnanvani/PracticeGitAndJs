8fc90186184444e4b575fc4f2a50dba0103298d6
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-20 17:57:59 +0530

    both solved

--- reverseArray.js
+++ reverseArray.js
@@ -13,6 +13,6 @@ const reverseArray = (arr, k) => {
         }
     }
 }
-const arr = [1, 2, 3, 4, 5, 6, 7];
-reverseArray(arr, 3)
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+reverseArray(arr, 4)
 console.log(arr);
