0a858975f8c0893faba294ef39c44c10d095ca88
Ravikant07 <ravikant.prasad@vidyamantra.com>
2022-10-20 21:28:50 +0530

    done

--- index2.js
+++ index2.js
@@ -4,16 +4,19 @@ let a = [16,17,4,3,5,2];
 let b = 0;
 let m =[];
  for(let i = 0; i< a.length;i++){
+    let condition = true;
     for(let j = i+1;j<a.length;j++){
-        if(a[i]>=a[j]){
-            b = a[i];
-            m.push(b);
+        if(a[j]>=a[i]){
+            condition = false;
+            break;
         //    b.push(a[i]);
         //    console.log(a[j]);
 
          }
           
      }
+if (condition == true) m.push(a[i]);
+
  
 
 }
