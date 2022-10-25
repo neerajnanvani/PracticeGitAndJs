9db4f9f6ec1599ec14286f644eafaf60619c3efd
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-25 18:51:43 +0530

    added commandline arguments

--- reverseArray.js
+++ reverseArray.js
@@ -1,10 +1,11 @@
 const reverseArray = (arr, k) => {
-    for (let i = 0; i < Math.ceil(arr.length / k); i++) {
-        let l = i * k;
-        let r = l + k - 1;
-        if (r >= arr.length) r = arr.length - 1;
-        console.log(l, r)
-        while (l < r) {
+    arr = JSON.parse(arr);
+    k = JSON.parse(k)
+    for (let i = 0; i < Math.ceil(arr.length / k); i++) {   // iterating by length of array divide by k i.e number of subsets
+        let l = i * k;  // indicates starting of current sub array
+        let r = l + k - 1;  // indicates end of current sub array
+        if (r >= arr.length) r = arr.length - 1;    // check if last sub array size is less than k
+        while (l < r) {     // exchanging value of items from left and right side of sub array
             arr[l] = arr[l] + arr[r];
             arr[r] = arr[l] - arr[r];
             arr[l] = arr[l] - arr[r];
@@ -14,3 +15,5 @@ const reverseArray = (arr, k) => {
     }
     return arr;
 }
+
+console.log(reverseArray(process.argv[2], process.argv[3]))
