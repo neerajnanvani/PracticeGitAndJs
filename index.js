d0d9de6b078a7fc0f58962d58f8ab62edad02ced
Ravikant07 <ravikant.prasad@vidyamantra.com>
2022-10-25 21:46:46 +0530

    seperated both answer

--- index.js
+++ index.js
@@ -31,25 +31,6 @@ let k = 3;
 console.log(arr);
 // ======================================================
 
-let a = [16,17,4,3,5,2];
-
-let b = 0;
-let m =[];
- for(let i = 0; i< a.length;i++){
-    for(let j = i+1;j<a.length;j++){
-        if(a[i]>=a[j]){
-            b = a[i];
-            m.push(b);
-        //    b.push(a[i]);
-        //    console.log(a[j]);
-
-         }
-          
-     }
- 
-
-}
-console.log(m);
  
   
  
