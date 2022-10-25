e41e800161d7656cf60b550a7d71d744dd81960d
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-25 18:42:51 +0530

    added command line args

--- findLeader.js
+++ findLeader.js
@@ -1,12 +1,16 @@
 const findLeaders = (arr) => {
+    arr = JSON.parse(arr);
+    console.log(arr.length)
     const ans = [];
     let max = arr[arr.length - 1];
-    ans.push(max);
-    for (let i = arr.length - 2; i >= 0; i--) {
-        if (arr[i] >= max) {
+    ans.push(max);  // pushing right most because it wil always be aleader
+    for (let i = arr.length - 2; i >= 0; i--) { // Iterating over array from right to left
+        if (arr[i] >= max) {    // if value more than max it means it is leader
             ans.push(arr[i]);
             max = arr[i];
         }
     }
     return ans;
 }
+
+console.log(findLeaders(process.argv[2]))
