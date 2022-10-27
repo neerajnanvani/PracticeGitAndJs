e320c42a750d309416c0453495c44321bdd27f1d
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-27 17:48:23 +0530

    remove wrongoutput bug

--- caeser-cipher.js
+++ caeser-cipher.js
@@ -3,8 +3,7 @@ const caeserCipher = (text, key) => {
     let ans = '';
     // looping string
     for (let i = 0; i < text.length; i++) {
-        console.log('Z'.charCodeAt(0))
-        if (text.charCodeAt(i) < 91 || text.charCodeAt(i) > 64)
+        if (text.charCodeAt(i) < 91 && text.charCodeAt(i) > 64)
             ans += String.fromCharCode((text.charCodeAt(i) - 65 + key) % 26 + 65);  // manipulating capital letters
         if (text.charCodeAt(i) < 97 || text.charCodeAt(i) > 122) continue;  // checking if character not a small case letter
         ans += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);  // manipulating small letters
