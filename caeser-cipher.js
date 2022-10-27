71a26591bc2bca5604a6efda2883f4cf08dcc970
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-27 17:46:43 +0530

    handle uppercase letters

--- caeser-cipher.js
+++ caeser-cipher.js
@@ -1,11 +1,13 @@
 const caeserCipher = (text, key) => {
     key = JSON.parse(key);
-    console.log('a'.charCodeAt(0))
     let ans = '';
     // looping string
     for (let i = 0; i < text.length; i++) {
+        console.log('Z'.charCodeAt(0))
+        if (text.charCodeAt(i) < 91 || text.charCodeAt(i) > 64)
+            ans += String.fromCharCode((text.charCodeAt(i) - 65 + key) % 26 + 65);  // manipulating capital letters
         if (text.charCodeAt(i) < 97 || text.charCodeAt(i) > 122) continue;  // checking if character not a small case letter
-        ans += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);  // adding changed character in string
+        ans += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);  // manipulating small letters
     }
     return ans;
 }
