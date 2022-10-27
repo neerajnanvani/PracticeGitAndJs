dd3e22bdf0db1b5470f1efa04c114df928085ab9
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-27 17:42:34 +0530

    added comments

--- caeser-cipher.js
+++ caeser-cipher.js
@@ -2,9 +2,10 @@ const caeserCipher = (text, key) => {
     key = JSON.parse(key);
     console.log('a'.charCodeAt(0))
     let ans = '';
+    // looping string
     for (let i = 0; i < text.length; i++) {
-        ans += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);
-        console.log((text.charCodeAt(i) + key) % 97 + 97)
+        if (text.charCodeAt(i) < 97 || text.charCodeAt(i) > 122) continue;  // checking if character not a small case letter
+        ans += String.fromCharCode((text.charCodeAt(i) - 97 + key) % 26 + 97);  // adding changed character in string
     }
     return ans;
 }
