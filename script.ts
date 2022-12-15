0b7132e3179493e45c06c227f1fc3e576c0efeba
neetu1123 <95036500+neetu1123@users.noreply.github.com>
2022-12-15 16:32:52 +0530

    Given a type of array

--- script.ts
+++ script.ts
@@ -3,7 +3,7 @@ let arr:number[]=[1,2,3,4,5,6]
         let index:number=3
         
         //function of reverseArray
-       const reverseArr= (arr,index) => {
+       const reverseArr= (arr:number[],index:number) => {
         for (let i = 0; i < arr.length; i += index) {
         let begin = i;
         let end = Math.min(i + index - 1, arr.length - 1);
