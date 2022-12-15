30773b01aee4ffa60bf668f83007d05d84a569ad
neetu1123 <95036500+neetu1123@users.noreply.github.com>
2022-12-15 14:55:03 +0530

    update with some changes

--- script.ts
+++ script.ts
@@ -1,16 +1,22 @@
 // array , in this find the reverse of array
 let arr:number[]=[1,2,3,4,5,6]
-        let index:number=4
+        let index:number=3
         
         //function of reverseArray
-        const reverseArr= (arr:( number)[],index:number) => {
-            let newArr:number
-            for(let i=0;i<=index;i+=3){
-            newArr =arr[+i]
-             arr[i]=arr[i+2]
-             arr[i+2]=newArr
-             
-            }
-         return arr
+       const reverseArr= (arr,index) => {
+        for (let i = 0; i < arr.length; i += index) {
+        let begin = i;
+        let end = Math.min(i + index - 1, arr.length - 1);
+        let temp;
+
+        while (start < end) {
+            temp = arr[begin];
+            arr[begin] = arr[end];
+            arr[end] = temp;
+            begin++;
+            end--;
+        }
+    }
+    return arr;
         }
         reverseArr(arr,index) // return reverse sub array
