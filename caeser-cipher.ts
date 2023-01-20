c5dd0b4195ee94fa1bb7a6eb5449116ec6a70d17
Mohd Saqib <mohd.saqib@vidyamantra.com>
2023-01-20 18:05:39 +0530

    created funtion for encryption

---
+++ caeser-cipher.ts
@@ -0,0 +1,21 @@
+
+function cipher(strInput: string, n: number): string {
+    const encryptedValue = strInput.split("").map((char) => {
+    // getting the character code of the current character..
+    let charCode = char.charCodeAt(0);
+    // if not an alphabet (a-z) return character unencrypted.
+    if (charCode < 97 || charCode > 122) return char;
+
+    // shift character code by 'n'.
+    let shiftedIndex = charCode + n;
+    // rotating back to the front alphabet if character code is greater than the last alphabet.
+    // 122 is the code for last alphabet 'z'.
+    if (shiftedIndex > 122) {
+      shiftedIndex = shiftedIndex - 122 + 96;
+    }
+    // getting the alphabet from character code..
+    return String.fromCharCode(shiftedIndex);
+  }).join("");
+
+  return encryptedValue;
+}
