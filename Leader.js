46817a308ddbacbbc6b0f6c6f6df2777989e5ceb
Nagesh Singh Tomer <nagesh.tomer@vidyamantra.com>
2023-02-15 11:34:37 +0530

    All changes made as disscused

--- Leader.js
+++ Leader.js
@@ -1,20 +1,20 @@
 const SIZE_OF_ARRAY = +process.argv[2];
 const STRING_ARRAY = process.argv[3].trim().split(',')
-const NUMBER_ARRAY = STRING_ARRAY.map((ele) => { return +ele })
-const TEMP_ARRAY = [...NUMBER_ARRAY]; 
+const NUMBER_ARRAY = STRING_ARRAY.map(ele => +ele)
+const TEMP_ARRAY = [...NUMBER_ARRAY];
 
 let leaderList = [];
 
 function isLeader(number, arr) {
-   let greaterThanEle = arr.filter((numInArr) => {
-      return numInArr > number
-   })
-   if (greaterThanEle.length > 0) return false
-   else return true
+   // filtering out those RHS number that are greater than current number  
+   // than returning filtered array length
+   return arr.filter(numInArr => numInArr > number).length
 }
 
 NUMBER_ARRAY.forEach((ele) => {
+   // poping out 0th index element every time, since we have to check only RHS number 
    TEMP_ARRAY.shift()
-   let leader = isLeader(ele, TEMP_ARRAY)
-   if (leader) leaderList.push(ele)
+   // if filtered array length is === 0 i.e false, than pushing it to leaderList array 
+   if (!isLeader(ele, TEMP_ARRAY)) leaderList.push(ele)
 });
+console.log(leaderList)
