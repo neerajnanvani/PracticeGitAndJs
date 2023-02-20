efa8faa7067ee761e54d630b4996c684b281f4a2
Nagesh Singh Tomer <nagesh.tomer@vidyamantra.com>
2023-02-20 13:25:09 +0530

    Ceaser Chiper Completed

---
+++ CaeserCipher.ts
@@ -0,0 +1,29 @@
+(() => {
+  let confidentialInformation: string;
+  confidentialInformation = "hello how are you ?";
+  const rotation: number = 4;
+
+  function cipherEncrypting(information: string, rotateBy: number): void {
+    let unencryptedInformation: string[]; // store incoming unencrypted information
+    let encryptedInformation: string[] = []; // store encrypted information
+    unencryptedInformation = information.split(""); // converting string into string Array
+
+    unencryptedInformation.forEach((char) => {
+      let num: number = char.charCodeAt(0); // convert char to number at index 0
+      // Logic for encryption
+      if (num >= 97 && num <= 122) {
+        num += rotateBy;
+        if (num <= 122) encryptedInformation.push(String.fromCharCode(num)); // convert number to char and pushing it.
+        else encryptedInformation.push(String.fromCharCode(num - 26));
+      } 
+      else encryptedInformation.push(String.fromCharCode(num));
+    });
+
+    // printing encrypted information
+    console.log(encryptedInformation.join(""));
+  }
+
+  if (confidentialInformation)
+    cipherEncrypting(confidentialInformation.toLocaleLowerCase(), rotation);
+  else console.log("Missing Confidential Information");
+})();
