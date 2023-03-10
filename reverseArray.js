78d0140f729ee433b1ff4661ab2b22abe3c75ac2
sahil <sahil@vidyamantra.com>
2023-03-10 17:35:17 +0530

    created reverseArr function

---
+++ reverseArray.js
@@ -0,0 +1,25 @@
+// ## Q1: Reverse array in groups.
+//   Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+
+// Note: If at any instance, there are no more subarrays of size greater than or equal to K, then reverse the last subarray (irrespective of its size).
+
+// Input & output:
+
+// Input:
+// N = 5, K = 3
+// arr[] = {1,2,3,4,5}
+// Output: 3 2 1 5 4
+// Explanation: First group consists of elements
+// 1, 2, 3. Second group consists of 4,5.
+
+// get sub-arrays 
+// make a function to reverse it 
+// reverse each sub array 
+// spread it into result array 
+function reverseArr(arr){
+    let result = []
+for(let i=arr.length;i>0;i--){
+result.push(arr.pop())
+}
+return result
+}
